8a868f041e99e2e666e78cfe02828d2ec867e3a8
martinportgas <martinportgas@gmail.com>
2023-06-11 20:14:32 +0700

    Part 1 cihuy

---
+++ Ardita.Models/DbModels/MstGmdDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ardita.Models.DbModels;
+
+public partial class MstGmdDetail
+{
+    public Guid GmdDetailId { get; set; }
+
+    public Guid GmdId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Unit { get; set; } = null!;
+
+    public Guid? CreatedBy { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public virtual MstGmd Gmd { get; set; } = null!;
+
+    public virtual ICollection<MstSubTypeStorageDetail> MstSubTypeStorageDetails { get; } = new List<MstSubTypeStorageDetail>();
+
+    public virtual ICollection<TrxArchive> TrxArchives { get; } = new List<TrxArchive>();
+
+    public virtual ICollection<TrxMediaStorage> TrxMediaStorages { get; } = new List<TrxMediaStorage>();
+
+    public virtual ICollection<TrxTypeStorageDetail> TrxTypeStorageDetails { get; } = new List<TrxTypeStorageDetail>();
+}
